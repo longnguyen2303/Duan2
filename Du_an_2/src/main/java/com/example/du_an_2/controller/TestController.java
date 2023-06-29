377de2be91fcc7b10e31dc533203a2aca08fff48
DESKTOP-T0368G0\fuoc <nxp132003@gmail.com>
2023-06-29 22:59:37 +0700

    trang sp

--- Du_an_2/src/main/java/com/example/du_an_2/controller/TestController.java
+++ Du_an_2/src/main/java/com/example/du_an_2/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.du_an_2.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.du_an_2.entities.ChiTietSP;
-
+import com.example.du_an_2.entities.Hang;
 import com.example.du_an_2.repositories.CTSPRepository;
+import com.example.du_an_2.repositories.HangRepository;
 
 @Controller
 @RequestMapping("sneaker")
 public class TestController {
 	@Autowired
 	CTSPRepository ctspRepository;
+	@Autowired
+	HangRepository hangRepository;
 
 	@GetMapping("trang-chu")
 	public String trangChu(Model model) {
@@ -41,18 +45,38 @@ public class TestController {
 	}
 
 	@GetMapping("product/search")
-	public String productSearch(Model model, @RequestParam("inputsearch") String input, @RequestParam(name = "page", defaultValue = "0") Integer pageNo) {
+	public String productSearch(Model model, @RequestParam("inputsearch") String input,
+			@RequestParam(name = "page", defaultValue = "0") Integer pageNo) {
 		Pageable pageable = PageRequest.of(pageNo, 4, Sort.by(Sort.Direction.ASC, "lastModifiedDate"));
 		Page<ChiTietSP> page = ctspRepository.getListBySearch(input, pageable);
 		model.addAttribute("listCTSP", page);
 		return "product";
 	}
 
-
 	@GetMapping("detail/{id}")
 	public String detail(Model model, @PathVariable UUID id) {
 		ChiTietSP ctChiTietSP = ctspRepository.findById(id).orElse(null);
 		model.addAttribute("detail", ctChiTietSP);
 		return "detail";
+	}
+
+	@GetMapping("productfilter")
+	public String filter1(Model model) {
+		List<Hang> list = hangRepository.findAll();
+		model.addAttribute("listHang", list);
+		List<ChiTietSP> list1 = ctspRepository.findAll();
+		model.addAttribute("list", list1);
+		return "productfilter";
+	}
+
+	@GetMapping("hang/{id}/product")
+	public String filter(Model model, @PathVariable UUID id) {
+		List<Hang> list = hangRepository.findAll();
+		model.addAttribute("listHang", list);
+
+		List<ChiTietSP> listbyHang = ctspRepository.getCTSPbyid(id);
+		model.addAttribute("listSpbyHang", listbyHang);
+
+		return "productfilter1";
 	}
 }
